ba0c38367d5a6e8baf36306cfb7c968bc2bd0b92
82109 <82109@DESKTOP-2DKUPIR>
2022-02-07 15:09:22 +0900

    adminOrderDAO,OrderDAO 제작

--- Jumo/src/main/java/jumo/admin/order/adminOrderDAO.java
+++ Jumo/src/main/java/jumo/admin/order/adminOrderDAO.java
@@ -1,5 +1,39 @@
 package jumo.admin.order;
 
-public class adminOrderDAO {
+import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository("adminOrderDAO")
+public class adminOrderDAO {
+	
+	@Resource(name="sqlSessionTemplate")
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	//전체 주문리스트
+	public List<Map<String, Object>> allList(Map<String, Object> map) throws Exception {
+		return sqlSessionTemplate.selectList("order.allList", map);
+	}
+	
+	//주문리스트 	
+	public List<Map<String, Object>> orderList(Map<String, Object> map) throws Exception {
+		return sqlSessionTemplate.selectList("order.orderList", map);
+	}
+	
+	//주문리스트 검색
+	public List<Map<String, Object>> orderListSearch(Map<String, Object> map) throws Exception {
+		return sqlSessionTemplate.selectList("order.orderListSearch", map);
+	}
+	//주문리스트 상세보기
+	public List<Map<String, Object>> selectOrderOId(Map<String, Object> map) throws Exception {
+		return sqlSessionTemplate.selectList("order.selectOrderOId", map);
+	}
+	//주문 정보 수정 기능
+	public List<Map<String, Object>> updateOrderId(Map<String, Object> map) throws Exception {
+		return sqlSessionTemplate.selectList("orderupdateOrderId", map); 
+	}
 }
--- Jumo/src/main/java/jumo/common/order/OrderDAO.java
+++ Jumo/src/main/java/jumo/common/order/OrderDAO.java
@@ -1,5 +1,42 @@
 package jumo.common.order;
 
-public class OrderDAO {
+import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository("OrderDAO")
+public class OrderDAO {
+		
+	@Resource(name="sqlSessionTemplate")
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	//상품 바로 구매 기능	
+	public List<Map<String, Object>> insertOrderDirect(Map<String, Object> map) throws Exception {
+		return sqlSessionTemplate.selectList("order.insertOrderDirect", map);
+	}
+	
+	//장바구니 상품 구매 폼
+	public List<Map<String, Object>> selectMemberId(Map<String, Object> map) throws Exception {
+		return sqlSessionTemplate.selectList("order.selectMemberId", map);
+	}
+	
+	//장바구니 상품 구매 기능
+	public List<Map<String, Object>> insertOrderBasket(Map<String, Object> map) throws Exception {
+		return sqlSessionTemplate.selectList("order.insertOrderBasket", map);
+	}
+	
+	//상품 구매 결과(selectOrderOId)
+	public List<Map<String, Object>> selectOrderOId(Map<String, Object> map) throws Exception {
+		return sqlSessionTemplate.selectList("order.selectOrderOId", map);
+	}
+	
+	//상품 구매 결과(selectOrderOBNumber)
+	public List<Map<String, Object>> selectOrderOBNumber(Map<String, Object> map) throws Exception {
+		return sqlSessionTemplate.selectList("order.selectOrderOBNumber", map);
+	}
+	
 }
