6740734b8687d5cf1d4e37fb7fd1003fffc44f35
qkffo <qkffo@LAPTOP-JGPV8TEF>
2022-02-07 16:25:58 +0900

    (Product,Basket)DAO

--- Jumo/src/main/java/jumo/admin/product/AdminProductDAO.java
+++ Jumo/src/main/java/jumo/admin/product/AdminProductDAO.java
@@ -1,5 +1,48 @@
 package jumo.admin.product;
 
-public class AdminProductDAO {
+import java.util.Map;
 
+import javax.annotation.Resource;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository("adminProductDAO")
+public class AdminProductDAO {
+	
+	@Resource(name="sqlSessionTemplate")
+	private SqlSessionTemplate sqlSessionTemplate;
+
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> allList() throws Exception{
+		return sqlSessionTemplate.selectList("product.allList");
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> allListKeyWordSearch(Map<String, Object> map) throws Exception{
+		return sqlSessionTemplate.selectList("product.allListKeyWordSearch",map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void insertProduct() throws Exception{
+		sqlSessionTemplate.insert("product.insertProduct");
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> selectProductId(Map<String, Object> map) throws Exception{
+		return sqlSessionTemplate.selectOne("product.selectProductId",map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void updateProduct() throws Exception{
+		sqlSessionTemplate.update("product.updateProduct");
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void deleteProduct() throws Exception{
+		sqlSessionTemplate.delete("product.deleteProduct");
+	}
+	
 }
--- Jumo/src/main/java/jumo/common/basket/BasketDAO.java
+++ Jumo/src/main/java/jumo/common/basket/BasketDAO.java
@@ -1,5 +1,29 @@
 package jumo.common.basket;
 
-public class BasketDAO {
+import java.util.Map;
 
+import javax.annotation.Resource;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository("basketDAO")
+public class BasketDAO {
+	
+	@Resource(name="sqlSessionTemplate")
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	public List<Map<String,Object>> basketList(Map<String,Object> map)throws Exception{
+		return sqlSessionTemplate.selectList("basket.basketList",map);
+	}
+	
+	public void updateBasket()throws Exception{
+		sqlSessionTemplate.update("basket.updateBasket");
+	}
+	
+	public void deleteBasket() throws Exception{
+		sqlSessionTemplate.delete("basket.deleteBasket");
+	}
 }
