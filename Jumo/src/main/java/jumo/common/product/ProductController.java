ca948ef5703ef69d591055848d9d9d6ac938bfe9
qkffo <qkffo@LAPTOP-JGPV8TEF>
2022-02-09 10:57:07 +0900

    ProductController 동작확인완료

--- Jumo/src/main/java/jumo/common/product/ProductController.java
+++ Jumo/src/main/java/jumo/common/product/ProductController.java
@@ -1,5 +1,71 @@
 package jumo.common.product;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class ProductController {
+	
+	@RequestMapping("/allList.al")
+	public String allList(Model model) throws Exception{
+		
+		return "/product/allList";
+	}
+	
+	@RequestMapping(value="/allList.al", method=RequestMethod.POST)
+	public String allListSearch(Model model) throws Exception{
+		
+		return "/product/allList";
+	}
+	
+	@RequestMapping("/aclList.al")
+	public String aclList(Model model) throws Exception{
+		
+		return "/product/aclList";
+	}
+	
+	@RequestMapping(value="/aclList.al", method=RequestMethod.POST)
+	public String aclListSearch(Model model) throws Exception{
+		
+		return "/product/aclList";
+	}
+	
+	@RequestMapping("/etcList.al")
+	public String etcList(Model model) throws Exception{
+		
+		return "/product/etcList";
+	}
+	
+	@RequestMapping(value="/etcList.al", method=RequestMethod.POST)
+	public String etcListSearch(Model model) throws Exception{
+		
+		return "/product/etcList";
+	}
+	
+	@RequestMapping("/pDetail.al")
+	public String pDetail(Model model) throws Exception{
+		
+		return "/product/pDetail";
+	}
+
+	@RequestMapping(value="/putBasket.al", method=RequestMethod.POST)
+	public String putBasket(Model model) throws Exception{
+		
+		return "/product/putBasket";
+	}
+	
+	@RequestMapping("/pReviewForm.al")
+	public String pReviewForm(Model model) throws Exception{
+		
+		return "/product/pReviewForm";
+	}
+	
+	@RequestMapping("/pReview.al")
+	public String pReview(Model model) throws Exception{
+		
+		return "/product/pReview";
+	}
 
 }
