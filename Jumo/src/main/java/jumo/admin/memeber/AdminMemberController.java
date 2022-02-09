43d5c27882f82cf4ef3683819ca9b672aaf0b35a
ny <ny@DESKTOP-IJH2AL3>
2022-02-09 11:05:57 +0900

    controller

--- Jumo/src/main/java/jumo/admin/memeber/AdminMemberController.java
+++ Jumo/src/main/java/jumo/admin/memeber/AdminMemberController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+//
 @Controller
 public class AdminMemberController {
 
