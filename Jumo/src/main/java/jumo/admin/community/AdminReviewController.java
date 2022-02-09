836c03f4b51e3cf7c5ede47e0e8598e8f4753960
xormr <xormr@DESKTOP-I4UVGS5>
2022-02-09 10:54:37 +0900

    CommunityController, AdminNoticeController, AdminReviewController

--- Jumo/src/main/java/jumo/admin/community/AdminReviewController.java
+++ Jumo/src/main/java/jumo/admin/community/AdminReviewController.java
@@ -1,5 +1,22 @@
 package jumo.admin.community;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AdminReviewController {
 
+	@RequestMapping(value="/adminReviewList.al")
+	public String adminReviewList(Model model) {
+		return "admin/community/adminReviewList";
+	}
+	
+	
+	@RequestMapping(value="/adminReviewDelete.al")
+	public String adminReviewDelete(Model model) {
+		return "admin/community/adminReviewDelete";
+	}
+	
+	
 }
--- Jumo/src/main/java/jumo/common/community/CommunityController.java
+++ Jumo/src/main/java/jumo/common/community/CommunityController.java
@@ -1,5 +1,48 @@
 package jumo.common.community;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class CommunityController {
 
+
+	@RequestMapping(value="/noticeList.al")
+	public String noticeList(Model model) {
+		return "community/noticeList";
+	}
+	
+	
+	@RequestMapping(value="/noticeDetail.al")
+	public String noticeDetail(Model model) {
+		return "community/noticeDetail";
+	}
+	
+	
+	@RequestMapping(value="/qnaList.al")
+	public String qnaList(Model model) {
+		return "community/qnaList";
+	}
+	
+	
+	@RequestMapping(value="/qnaDetail.al")
+	public String qnaDetail(Model model) {
+		return "community/qnaDetail";
+	}
+	
+	
+	@RequestMapping(value="/qnaForm.al")
+	public String qnaForm(Model model) {
+		return "community/qnaForm";
+	}
+	
+	
+	@RequestMapping(value="/qna.al")
+	public String qna(Model model) {
+		return "community/qna";
+	}
+	
+	
+	
 }
