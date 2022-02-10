2894a09fe2151336bc9d1a6ef8941112d96db305
qkffo <qkffo@LAPTOP-JGPV8TEF>
2022-02-10 11:30:57 +0900

    AdminOrder,Review Controller

--- Jumo/src/main/java/jumo/admin/community/AdminReviewController.java
+++ Jumo/src/main/java/jumo/admin/community/AdminReviewController.java
@@ -1,20 +1,44 @@
 package jumo.admin.community;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jumo.model.CommunityBean;
+import jumo.util.MapToBean;
+
 @Controller
 public class AdminReviewController {
+	
+	@Resource(name="adminCommunityService")
+	private AdminCommunityService adminCommunityService;
 
 	@RequestMapping(value="/adminReviewList.al")
-	public String adminReviewList(Model model) {
-		return "admin/community/adminReviewList";
+	public String adminReviewList(Model model) throws Exception{
+		List<Map<String, Object>> list = adminCommunityService.reviewList();
+		
+		List<CommunityBean> communityBeanList = new ArrayList<CommunityBean>();
+		
+		for(Map<String,Object> mapObject : list) {
+			communityBeanList.add(MapToBean.mapToCommunity(mapObject));
+		}
+		
+		model.addAttribute("communityBeanList", communityBeanList);
+		
+		return "adminReviewList";
 	}
 	
 	
 	@RequestMapping(value="/adminReviewDelete.al")
-	public String adminReviewDelete(Model model) {
+	public String adminReviewDelete(CommunityBean community, Model model) throws Exception{
+		adminCommunityService.deleteCommunityId(community);
+		
 		return "admin/community/adminReviewDelete";
 	}
 	
