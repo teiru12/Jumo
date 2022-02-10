e335b5ad2db900ab0ac26ae57a096acf6e57e573
xormr <xormr@DESKTOP-I4UVGS5>
2022-02-10 13:23:36 +0900

    adminproductController,adminmemberController

--- Jumo/src/main/java/jumo/admin/memeber/AdminMemberController.java
+++ Jumo/src/main/java/jumo/admin/memeber/AdminMemberController.java
@@ -1,36 +1,80 @@
 package jumo.admin.memeber;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//
+import jumo.model.MemberBean;
+import jumo.util.MapToBean;
+
+
 @Controller
 public class AdminMemberController {
 
+	@Resource(name="adminMemberService")
+	private AdminMemberService adminMemberService;
+
+	
 	@RequestMapping(value = "/memberList.al")
-	public String memeberList (Model model) {
-		return "admin/member/memberList";
+	public String memberList (Model model) throws Exception {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		List<MemberBean> memberBeanList = new ArrayList<MemberBean>();
+		list = adminMemberService.memberList(); 
+		
+		for(Map<String, Object> mapObject : list) {
+			memberBeanList.add(MapToBean.mapToMember(mapObject));
+		}
+		
+		model.addAttribute("memberBeanList", memberBeanList);
+		
+		return "memberList";
 	}
 	
 	@RequestMapping(value = "/memberList.al", method = RequestMethod.POST)
-	public String memberSearch (Model model) {
-		return "admin/member/memberList";
+	public String memberSearch (MemberBean member, Model model) throws Exception {
+		Map<String, Object> map = new HashMap<String, Object>();
+		MemberBean memberBean = new MemberBean();
+		
+		map = adminMemberService.selectMemberId(member);
+		
+		model.addAttribute("memberBean", memberBean);
+
+		return "memberList";
 	}
 	
 	@RequestMapping(value = "/memberDetail.al")
-	public String memberDetail (Model model) {
-		return "admin/member/memberDetail";
+	public String memberDetail (MemberBean member, Model model) throws Exception {
+		Map<String, Object> map = new HashMap<String, Object>();
+		MemberBean memberBean = new MemberBean();
+		
+		map = adminMemberService.selectMemberId(member);
+		
+		model.addAttribute("memberBean", memberBean);
+		
+		return "memberDetail";
 	}
 	
 	@RequestMapping(value = "/memberModify.al")
-	public String memberModify (Model model) {
+	public String memberModify (MemberBean member) throws Exception {
+
+		adminMemberService.updateMemberAdmin(member);
+		
 		return "admin/member/memberModify";
 	}
 	
 	@RequestMapping(value = "/memberDelete.al")
-	public String memberDelete (Model model) {
+	public String memberDelete (Model model, MemberBean member) throws Exception {
+		
+		adminMemberService.deleteMember(member);
+		
 		return "admin/member/memberDelete";
 	}
 	
