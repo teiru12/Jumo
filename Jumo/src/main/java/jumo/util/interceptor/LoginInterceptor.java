fa1edebae0b9990e7e06cc4fec38d590960ea37c
jonggyun <pacific1212@naver.com>
2022-02-22 09:47:03 +0900

    인터셉터 작성중

--- Jumo/src/main/java/jumo/util/interceptor/LoginInterceptor.java
+++ Jumo/src/main/java/jumo/util/interceptor/LoginInterceptor.java
@@ -15,18 +15,27 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 		
 		String email = (String) request.getSession().getAttribute("EMAIL");
 		
+		/* 로그인하지 않아도 사용할 수 있는 기능들 */
+		// main.al loginForm.al login.al joinForm.al joinSuccess.al 
+		// findId.al findIdResult.al findPw.al findPwResult.al confirmId.al
+		// allList.al aclList.al etcList.al pDetail.al
+		// noticeList.al noticeDetial.al qnaList.al qnaDetail.al
+		
 		/* 로그인하지 않은 사용자 핸들 */
 		if(email == null) {
 			System.out.println("1");
 			response.sendRedirect("/Jumo/main.al");
 			return false;
-		} else if(! email.equals("ADMIN")) {
-			System.out.println("2");
-			response.sendRedirect("/Jumo/main.al");
-			return false;
 		} else {
-			System.out.println("3");
-			return super.preHandle(request, response, handler);
+			if(! email.equals("ADMIN")) { 
+
+				System.out.println("2");
+				response.sendRedirect("/Jumo/main.al");
+				return false;
+			} else {
+				System.out.println("3");
+				return super.preHandle(request, response, handler);
+			}
 		}
 	}
 
