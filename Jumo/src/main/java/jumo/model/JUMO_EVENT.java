0109722fa16d76e7bc0ec5f54dc14adddb91900f
jonggyun <pacific1212@naver.com>
2022-02-28 12:31:30 +0900

    DD

--- Jumo/src/main/java/jumo/model/JUMO_EVENT.java
+++ Jumo/src/main/java/jumo/model/JUMO_EVENT.java
@@ -2,4 +2,68 @@ package jumo.model;
 
 public class JUMO_EVENT {
 
+	private int EVENT_KEY;
+	private String EMAIL;
+	private int JUMO_POINT;
+	private String COUPON1K;
+	private String COUPON2K;
+	private String COUPON3K;
+	private String COUPON5K;
+	private String COUPON10K;
+	private String RULLETDATE;
+	
+	public int getEVENT_KEY() {
+		return EVENT_KEY;
+	}
+	public void setEVENT_KEY(int eVENT_KEY) {
+		EVENT_KEY = eVENT_KEY;
+	}
+	public String getEMAIL() {
+		return EMAIL;
+	}
+	public void setEMAIL(String eMAIL) {
+		EMAIL = eMAIL;
+	}
+	public int getJUMO_POINT() {
+		return JUMO_POINT;
+	}
+	public void setJUMO_POINT(int jUMO_POINT) {
+		JUMO_POINT = jUMO_POINT;
+	}
+	public String getCOUPON1K() {
+		return COUPON1K;
+	}
+	public void setCOUPON1K(String cOUPON1K) {
+		COUPON1K = cOUPON1K;
+	}
+	public String getCOUPON2K() {
+		return COUPON2K;
+	}
+	public void setCOUPON2K(String cOUPON2K) {
+		COUPON2K = cOUPON2K;
+	}
+	public String getCOUPON3K() {
+		return COUPON3K;
+	}
+	public void setCOUPON3K(String cOUPON3K) {
+		COUPON3K = cOUPON3K;
+	}
+	public String getCOUPON5K() {
+		return COUPON5K;
+	}
+	public void setCOUPON5K(String cOUPON5K) {
+		COUPON5K = cOUPON5K;
+	}
+	public String getCOUPON10K() {
+		return COUPON10K;
+	}
+	public void setCOUPON10K(String cOUPON10K) {
+		COUPON10K = cOUPON10K;
+	}
+	public String getRULLETDATE() {
+		return RULLETDATE;
+	}
+	public void setRULLETDATE(String rULLETDATE) {
+		RULLETDATE = rULLETDATE;
+	}	
 }
--- Jumo/src/main/java/jumo/model/JUMO_POINT.java
+++ Jumo/src/main/java/jumo/model/JUMO_POINT.java
@@ -1,5 +1,36 @@
 package jumo.model;
 
+import java.util.Date;
+
 public class JUMO_POINT {
 
+	private int POINT_KEY;
+	private String EMAIL;
+	private int JUMO_POINT;
+	private Date RULLETDATE;
+	
+	public int getPOINT_KEY() {
+		return POINT_KEY;
+	}
+	public void setPOINT_KEY(int pOINT_KEY) {
+		POINT_KEY = pOINT_KEY;
+	}
+	public String getEMAIL() {
+		return EMAIL;
+	}
+	public void setEMAIL(String eMAIL) {
+		EMAIL = eMAIL;
+	}
+	public int getJUMO_POINT() {
+		return JUMO_POINT;
+	}
+	public void setJUMO_POINT(int jUMO_POINT) {
+		JUMO_POINT = jUMO_POINT;
+	}
+	public Date getRULLETDATE() {
+		return RULLETDATE;
+	}
+	public void setRULLETDATE(Date rULLETDATE) {
+		RULLETDATE = rULLETDATE;
+	}	
 }
