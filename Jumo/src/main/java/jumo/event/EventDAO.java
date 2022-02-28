92f0de55cc0597c48bff09d0d426496b22059a59
jonggyun <pacific1212@naver.com>
2022-02-28 12:34:58 +0900

    DAO 기초

--- Jumo/src/main/java/jumo/event/EventDAO.java
+++ Jumo/src/main/java/jumo/event/EventDAO.java
@@ -1,5 +1,15 @@
 package jumo.event;
 
+import javax.annotation.Resource;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository("eventDAO")
 public class EventDAO {
 
+	@Resource(name="sqlSessionTemplate")
+	SqlSessionTemplate sqlSessionTemplate;
+	
+	
 }
